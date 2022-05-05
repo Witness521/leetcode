190ac99b2b5934bcc37fb74b4ce5b19cd02a79f2
Witness <cfy123321>
2022-05-05 16:38:37 +0800

    5月4日  232

---
+++ 入门集合刷题/栈/232栈实现队列.py
@@ -0,0 +1,36 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack_in = []
+        self.stack_out = []
+
+    def push(self, x: int) -> None:
+        self.stack_in.append(x)
+
+    def pop(self) -> int:
+        # 如果stack_out中有元素，就从stack_out中pop
+        if self.empty():  # 如果为空
+            return None
+        if self.stack_out:
+            return self.stack_out.pop()
+        else:
+            for i in range(len(self.stack_in)):
+                self.stack_out.append(self.stack_in.pop())
+            return self.stack_out.pop()
+
+
+    def peek(self) -> int:
+        pop = self.pop()
+        self.stack_out.append(pop)
+        return pop
+
+    def empty(self) -> bool:
+        return not (self.stack_in or self.stack_out)
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
