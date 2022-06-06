4373c8afbd34e5f3b9d64b66ff9ff0d859b62c44
Witness521 <2320815132@qq.com>
2022-06-06 20:43:02 +0800

    6.6 19 在操作链表的时候 如果涉及到头节点的前驱 就要设置一个哑节点！  栈的方法

--- HOT100/19删除链表的倒数第 N 个结点.py
+++ HOT100/19删除链表的倒数第 N 个结点.py
@@ -9,18 +9,37 @@ class ListNode:
         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        # 统计链表的长度
-        listLen = 0
-        head_pointer = head
-        while head_pointer:
-            listLen += 1
-            head_pointer = head_pointer.next
-        '''添加哑节点，解决头节点没有前驱节点的问题'''
-        dummy = ListNode(0, head)  # 为头节点添加哑节点
+        '''中心思想：添加哑节点 就不会出现首节点前驱的糟心事'''
+
+        # 1. 统计链表的长度
+        # listLen = 0
+        # head_pointer = head
+        # while head_pointer:
+        #     listLen += 1
+        #     head_pointer = head_pointer.next
+        # '''添加哑节点，解决头节点没有前驱节点的问题'''
+        # dummy = ListNode(0, head)  # 为头节点添加哑节点
+        # head_pointer = dummy
+        # # 快慢指针解决问题
+        # for _ in range(listLen - n):
+        #     head_pointer = head_pointer.next
+        # head_pointer.next = head_pointer.next.next
+        # return head
+
+        # 2. 使用栈的方法
+        dummy = ListNode(0, head)
         head_pointer = dummy
-        # 快慢指针解决问题
-        for _ in range(listLen - n):
+        stack = []
+        while head_pointer:
+            stack.append(head_pointer)
+            head_pointer = head_pointer.next
+        for i in range(n):
+            stack.pop()
+        # 待删除元素的左元素
+        element_left = stack.pop()
+        head_pointer = dummy
+        while head_pointer != element_left:
             head_pointer = head_pointer.next
         head_pointer.next = head_pointer.next.next
-        return head
+        return dummy.next
 
